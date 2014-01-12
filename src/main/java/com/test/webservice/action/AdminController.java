4b96a86a11d394922037bf86a7b186e82be9524e
proshad <proshad@cefalo.no>
2014-01-12 22:32:13 +0600

    code refactor

--- src/main/java/com/test/webservice/action/AdminController.java
+++ src/main/java/com/test/webservice/action/AdminController.java
@@ -1,5 +1,14 @@
 package com.test.webservice.action;
 
+import com.generic.entity.Category;
+import com.generic.service.CategoryService;
+import com.generic.service.ProductRateService;
+import com.generic.service.ProductService;
+import com.generic.service.RateService;
+import com.test.webservice.util.HibernateUtil;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * User: Proshad
@@ -16,8 +29,32 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/admin")
 public class AdminController {
 
-    @RequestMapping(value = "/category/", method = RequestMethod.GET)
-    public String manageCategory(ModelMap map, HttpServletRequest request) throws Exception {
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRateService productRateService;
+
+    @Autowired
+    private RateService rateService;
+
+
+    @RequestMapping(value = "/category")
+    public String category(ModelMap map) throws Exception {
+        JSONArray jArrayCategories = new JSONArray();
+        List<Category> categories = categoryService.listOfCategory();
+        for (Category category : categories) {
+            JSONObject obj = new JSONObject();
+            obj.put("catID", category.getCategoryID());
+            obj.put("name", category.getCategoryName().trim());
+            jArrayCategories.add(obj);
+        }
+
+        map.put("categories",jArrayCategories);
+
         return "index";
     }
 }
